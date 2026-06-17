762c5b920c8b31aaf1caa83e2abee8b0bed7490d
swagatsarkar <76607441+swagatsarkar@users.noreply.github.com>
2026-06-17 13:52:30 +0530

    basic mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,53 @@
 import { useState } from "react";
 
 function App() {
- const [url, setUrl] = useState("");
- const [shortUrl, setShortUrl] = useState("");
+  const [url, setUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
 
- const shortenUrl = async () => {
-  try {
-    const response = await fetch(
-      "http://localhost:5000/api/shorten",
-      {
+  const shortenUrl = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/api/shorten", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          url: url
-        })
-      }
-    );
+          url: url,
+        }),
+      });
 
-    const data = await response.json();
+      const data = await response.json();
 
-    setShortUrl(
-      `http://localhost:5000/${data.shortCode}`
-    );
-  } catch (error) {
-    console.error(error);
-  }
- };
+      setShortUrl(`http://localhost:5000/${data.shortCode}`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
- return (
-  <div style={{ padding: "40px" }}>
-    <h1>URL Shortener</h1>
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>URL Shortener</h1>
 
-    <input
-      type="text"
-      placeholder="Enter URL"
-      value={url}
-      onChange={(e) => setUrl(e.target.value)}
-    />
+      <input
+        type="text"
+        placeholder="Enter URL"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
 
-    <button onClick={shortenUrl}>
-      Shorten
-    </button>
+      <button onClick={shortenUrl}>Shorten</button>
 
-    <br />
-    <br />
+      <br />
+      <br />
 
-    {shortUrl && (
-      <div>
-        <strong>Short URL:</strong>
-        <p>{shortUrl}</p>
-      </div>
-    )}
-  </div>
- );
+      {shortUrl && (
+        <div>
+          <strong>Short URL:</strong>
+          <p>{shortUrl}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,21 @@
+require("dotenv").config()
+// console.log(process.env.MONGO_URI);
+
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const { nanoid } = require("nanoid");
+
+const Url = require("./models/Url");
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
@@ -17,16 +32,49 @@ app.get("/api/test", (req, res) => {
   });
 });
 
-app.post("/api/shorten", (req, res) => {
-  const { url } = req.body;
+app.post("/api/shorten", async (req, res) => {
+  try {
+    const { url } = req.body;
 
-  const shortCode = nanoid(6)
+    const shortCode = nanoid(6);
 
-  res.json({
-    originalUrl: url,
-    shortCode
-  });
+    const newUrl = await Url.create({
+      originalUrl: url,
+      shortCode: shortCode,
+    });
+
+    res.json(newUrl);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
 });
+
+app.get("/:shortCode", async (req, res) => {
+  try {
+    const { shortCode } = req.params;
+
+    const url = await Url.findOne({ shortCode });
+
+    if (!url) {
+      return res.status(404).send("URL not found");
+    }
+    
+    res.redirect(url.originalUrl);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send("Server Error");
+  }
+});
+
+// app.get("/api/urls", async (req, res) => {
+//   const urls = await Url.find();
+//   res.json(urls);
+// });
 
 const PORT = 5000;
 
